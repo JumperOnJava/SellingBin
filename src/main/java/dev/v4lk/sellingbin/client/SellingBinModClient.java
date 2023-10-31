18f3fc33a914e7bdd97918363f24238dfefc3624
ipz233_vvv <jumpergooog@gmail.com>
2023-10-31 18:11:59 +0200

    fixed crashes

--- src/main/java/dev/v4lk/sellingbin/client/SellingBinModClient.java
+++ src/main/java/dev/v4lk/sellingbin/client/SellingBinModClient.java
@@ -2,6 +2,7 @@ package dev.v4lk.sellingbin.client;
 
 import dev.v4lk.sellingbin.ConfigSynchronizer;
 import dev.v4lk.sellingbin.Trade;
+import io.github.jumperonjava.multitooltipapi.MutliTooltipApi;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.TooltipComponentCallback;
@@ -9,6 +10,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.TooltipComponentCallback;
 public class SellingBinModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        new MutliTooltipApi().initialize();
         ClientPlayConnectionEvents.INIT.register(ConfigSynchronizer::client);
         TooltipComponentCallback.EVENT.register(data -> {
             if(data instanceof Trade trade) {
